33939cb8663d93873b00e366c587ceff46834812
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-18 16:30:50 +0200

    upgrade help util

--- cmd/utilHelp.go
+++ cmd/utilHelp.go
@@ -5,44 +5,24 @@ package cmd
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import (
-	"github.com/ttacon/chalk"
+	"github.com/DanielRivasMD/domovoi"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// formatHelp produces the “help” header + description.
-//
-//	author: name, e.g. "Daniel Rivas"
-//	email:  email, e.g. "danielrivasmd@gmail.com"
-//	desc:   the multi‐line description, "\n"-separated.
-func formatHelp(author, email, desc string) string {
-	header := chalk.Bold.TextStyle(
-		chalk.Green.Color(author+" "),
-	) +
-		chalk.Dim.TextStyle(
-			chalk.Italic.TextStyle("<"+email+">"),
-		)
-
-	// prefix two newlines to your desc, chalk it cyan + dim it
-	body := "\n\n" + desc
-	return header + chalk.Dim.TextStyle(chalk.Cyan.Color(body))
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-var helpRoot = formatHelp(
+var helpRoot = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Interpret hotkeys into markdown keyboard visuals",
 )
 
-var helpDisplay = formatHelp(
+var helpDisplay = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
-	"Scan EDN metadata + vector rules & emit a 4-column Markdown table",
+	"Scan EDN metadata & output Markdown table",
 )
 
-var helpInterpret = formatHelp(
+var helpInterpret = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"<danielrivasmd@gmail.com>",
 	"",
