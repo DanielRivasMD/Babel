2df586e66fba6f4dd0ebd4fcbab45e90e956c18a
Daniel Rivas <danielrivasmd@gmail.com>
2026-06-17 13:47:27 +0300

    update embed cmd

---
+++ src/cmd/embed.rs
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use anyhow::{Context, Result as anyResult, bail};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use crate::cli::GlobalOpts;
+use crate::forge;
+use crate::lookup::Lookups;
+use crate::parse;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn run(global: GlobalOpts, target: Option<String>) -> anyResult<()> {
+    let lookups = Lookups::load(&global)?;
+    let target = target
+        .or_else(|| global.program.clone())
+        .context("no target specified")?;
+    if global.program.is_none() {
+        bail!("`--program` is required");
+    }
+    let paths = parse::resolve_edn_files(None, &global.root);
+    let all_entries = parse::parse_edn_files(&paths)?;
+    forge::embed_config(all_entries, &target, &lookups, &global)?;
+    Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
