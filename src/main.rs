cf9d345796ff6bce26056b914cf032fec0ced42d
Daniel Rivas <danielrivasmd@gmail.com>
2026-06-17 13:47:47 +0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
-mod cli;
-mod cmds;
-mod forger;
-mod lookup;
-mod parser;
-mod util;
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+use anyhow::Result as anyResult;
 use clap::Parser;
 
-fn main() -> anyhow::Result<()> {
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use cli::Command;
+use cli::GlobalOpts;
+use cmd::{completion, compose, construct, display, embed, identity, interpret};
+use util::default_root_dir;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+mod cli;
+mod cmd;
+mod forge;
+mod lookup;
+mod parse;
+mod util;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fn main() -> anyResult<()> {
     let cli = cli::Cli::parse();
-    cmds::run(cli)
+    let global = GlobalOpts {
+        verbose: cli.verbose,
+        program: cli.program,
+        root: cli.root.unwrap_or_else(default_root_dir),
+    };
+    match cli.command {
+        Command::Construct => construct::run(global)?,
+        Command::Compose { template } => compose::run(global, template)?,
+        Command::Display { file, render, sort } => display::run(global, file, render, sort)?,
+        Command::Embed { target } => embed::run(global, target)?,
+        Command::Interpret { target } => interpret::run(global, target)?,
+        Command::Identity => identity::run()?,
+        Command::Completion { shell } => completion::run(shell)?,
+    }
+    Ok(())
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
