15145257ad32c5306bcde48a8482e661a3fb0eda
Daniel Rivas <danielrivasmd@gmail.com>
2026-06-17 13:46:41 +0300

    deprecate completion cmd

--- src/cmds/completion.rs
+++
@@ -1,8 +0,0 @@
-use clap::CommandFactory;
-use clap_complete::{generate, Shell};
-use std::io;
-
-pub fn run(shell: Shell) {
-    let mut cmd = crate::cli::Cli::command();
-    generate(shell, &mut cmd, "babel", &mut io::stdout());
-}
